32e35c6b41b751e84fef6093da00da498876ae91
Nuve17 <simon.chavarnac@gmail.com>
2017-10-13 10:43:14 +0200

    Add Parse features

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+from pyPdf import PdfFileWriter, PdfFileReader
+from os import rename, listdir, path, makedirs
+from datetime import datetime, date
+import arrow
+import glob
+import sys
 import requests
 import yaml
 import re
 
-config = yaml.load(open("./config.yml"))
+config = yaml.load(open("../config.yml"))
 
+
+def change_name(PdfDescriptor):
+    reader = PdfFileReader(PdfDescriptor)
+    contents = reader.getPage(0).extractText()
+    year=contents.split('ESGI')[1][-7:][1:5]
+    date=contents.split('lundi ')[1][:5]+"/"+year
+    return date.replace('/','-')
 
 def get_cookies():
     rx_lt = 'value="[a-zA-Z0-9\-]*"'
@@ -26,7 +39,43 @@ def get_pdf(url, cookies):
         return str(e)
 
 
-def split_pdf(path):
+def split_pdf(path_pdf):
+
+    inputpdf = PdfFileReader(file(path_pdf, "rb"))
+    inputpdf.decrypt('')
+
+    if not path.exists('./tmp'):
+        makedirs('./tmp/')
+
+    for i in range(inputpdf.numPages // 1):
+        output = PdfFileWriter()
+        output.addPage(inputpdf.getPage(i * 1))
+        if i * 2 + 1 <  inputpdf.numPages:
+            output.addPage(inputpdf.getPage(i))
+        newname =path_pdf[:7] + "-" + str(i) + ".pdf"
+        outputStream = file( "./tmp/"+newname, "w+")
+        output.write(outputStream)
+        DateName=change_name(outputStream)
+        outputStream.close()
+        rename("./tmp/"+newname, "./tmp/"+DateName)
+
+    AllPdf=listdir('./tmp/')
+    now=datetime.now()
+    now=now.strftime('%Y-%m-%d')
+    DateNow=arrow.get(now)
+
+    for pdf in AllPdf:
+        PdfReturn=pdf.split('-')
+        PdfReturn=PdfReturn[::-1]
+        PdfDate='-'.join(PdfReturn)
+
+        DateFile=arrow.get(pdf)
+        print DateNow
+        print DateFile
+        delta= (DateFile-DateNow)
+        print now
+        print PdfDate
+        print delta.days
 
     return True
 
@@ -52,7 +101,7 @@ def main():
         if rx_planning.search(student_page.text):
             pdf_link = rx_planning.findall(html)[0][6:-19]
             filename = get_pdf(pdf_link, cookies)
-
+            pdf_recent=split_pdf(filename)
 
 if __name__ == '__main__':
     main()
